21e02ec528833c613f82e57624202ee9a49a9b1a
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-05-06 17:55:12 -0500

    first commit

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const siteTitle = 'Full Stack Banking App';
+
+export default function Layout({ children }) {
+  return (
+    <div className={inter.className}>{children}</div>
+  )
+}
